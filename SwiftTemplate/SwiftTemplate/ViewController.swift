aa1c5ee9568fd5ab2e952c442f00c50907b2fd3c
Sonny Fournier <sfournier@big-boss-studio.com>
2020-03-12 15:55:23 +0100

    :tada: Project creation

---
+++ SwiftTemplate/SwiftTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTemplate
+//
+//  Created by Sonny Fournier on 12/03/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
