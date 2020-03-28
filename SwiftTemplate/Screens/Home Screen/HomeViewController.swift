29ffdff076838c67ff45cf3d9faaa9ba0c99c330
Sonny Fournier <sfournier@big-boss-studio.com>
2020-03-28 17:44:04 +0100

    :sparkles: Add UILabel as example

--- SwiftTemplate/Screens/Home Screen/HomeViewController.swift
+++ SwiftTemplate/Screens/Home Screen/HomeViewController.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
 
+    // MARK: - Properties
+    let label = UILabel()
+
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.addSubviews(label)
+
+        setupUI()
+        setupConstraints()
+    }
+
+    // MARK: - UI setup
+    func setupUI() {
         view.backgroundColor = .systemBackground
+
+        label.text = "Welcome to SwiftTemplate"
+    }
+
+    // MARK: - Constraints setup
+    func setupConstraints() {
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
     }
 
 }
