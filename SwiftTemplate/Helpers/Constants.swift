b8618dafd247cf56f6c9d87b9df75a1f411204b5
Sonny <sonny.fournier@protonmail.com>
2021-09-07 13:16:50 +0200

    💄 Add design constants

--- SwiftTemplate/Helpers/Constants.swift
+++ SwiftTemplate/Helpers/Constants.swift
@@ -5,9 +5,22 @@
 //  Created by Sonny Fournier on 12/03/2020.
 //
 
-import Foundation
+import UIKit
 
 struct Constants {
+
+    // MARK: - Radius
+    struct Radius {
+        static let medium: CGFloat = 10
+        static let large: CGFloat = 20
+    }
+
+    // MARK: - Inset
+    struct Inset {
+        static let small: CGFloat = 10
+        static let medium: CGFloat = 15
+        static let large: CGFloat = 20
+    }
 
     // MARK: - Preferences
     struct Preferences {
